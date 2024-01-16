c7fdf229d623962061445712b407979d75751b22
ShansZhou <476993098@qq.com>
2024-01-16 14:56:20 +0800

    decode bbox: todo

--- predict.py
+++ predict.py
@@ -23,8 +23,8 @@ yolo.load_state_dict(torch.load(model_path, map_location=device))
 
 # read a image and preprocess
 img_path = "data/VOC2007/JPEGImages/000001.jpg"
-image_shape = cv.imread(img_path, cv.IMREAD_COLOR)
-im_rgb = cv.cvtColor(image_shape, cv.COLOR_BGR2RGB)
+im_src = cv.imread(img_path, cv.IMREAD_COLOR)
+im_rgb = cv.cvtColor(im_src, cv.COLOR_BGR2RGB)
 
 in_H = input_shape[0]
 in_W = input_shape[1]
@@ -49,7 +49,39 @@ outputs = yolo.forward(tensor_NCHW)
 boxes_withInfo = yolo.decodeBoxWithInfo(outputs)
 
 results = yolo.nonMaxSuppression(boxes_withInfo)
+i=0
+print("objects num: %d" % len(results[i]))
 
+# convert ratio of x,y,w,h to the scale of source image
+input_shape = np.array(input_shape)
+image_shape = np.array([im_H, im_W])
+new_shape = np.round(image_shape * np.min(input_shape/image_shape))
+offset  = (input_shape - new_shape)/2./input_shape
+scale   = input_shape/new_shape
+
+
+disp_img = im_src
+for bbox in results[i]:
+    
+    box_xy = bbox[0:2]
+    box_wh = bbox[2:4]
+    
+    box_xy  = (box_xy - offset[::-1]) * scale[::-1]
+    box_wh *= scale
+    
+    box_xy *= image_shape
+    box_wh *= image_shape
+    
+    tpL     = box_xy - (box_wh / 2.)
+    btR     = box_xy + (box_wh / 2.)
+    
+    tpL = np.int16(tpL)
+    btR = np.int16(btR)
+    
+    cv.rectangle(disp_img,tpL,btR,(0,255,0),2)
+    
+cv.imshow("yolo detect",disp_img)
+cv.waitKey(0)
 
 
 
